a7a37ff7f988d77d0e34ee82e1af9fe8a4ce6d19
jinny-do <sonmojiri7@gmail.com>
2026-08-07 13:09:18 +0900

    first commit

---
+++ src/main/java/com/msa4lmsv2academic/Msa4LmsV2AcademicApplication.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4LmsV2AcademicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4LmsV2AcademicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa4lmsv2academic/Msa4LmsV2AcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4LmsV2AcademicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
